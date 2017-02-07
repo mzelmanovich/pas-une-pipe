c5a841e9efede40353c4ceeef6f445d6745d9841
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-02-07 16:51:13 -0500

    set up for webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+        entry: './src/index',
+        output: {
+            path: './dist',
+            filename: 'index.js'
+        },
+        module: {
+            loaders: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }]
+        }
